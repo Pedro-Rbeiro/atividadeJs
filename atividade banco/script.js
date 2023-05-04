261869fe09284e582d46eb820bf3a6ffdf7d4ea0
Pedro-Rbeiro <business.psycho@outlook.com>
2023-05-04 10:53:23 -0300

    Atividade banco update

--- atividade banco/script.js
+++ atividade banco/script.js
@@ -1,24 +1,30 @@
 const numeroDiv = document.getElementById('resultado')
 const extratoDiv = document.getElementById("extrato")
-const today = new Date();
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-let day = today.getUTCDate()+"/"+month[today.getMonth()]+"/"+today.getFullYear()
-let hours = today.getHours()+":"+ today.getMinutes()+":"+today.getSeconds();
+let day = ""
+let hours = ""
 let saldo = 500
 numeroDiv.innerHTML=`
 <span><p> Saldo atual: <strong>R$${saldo}</strong></span> <br>
 `
+function time(){
+    const today = new Date();
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    day = today.getUTCDate()+"/"+month[today.getMonth()]+"/"+today.getFullYear()
+    hours = today.getHours()+":"+ today.getMinutes()+":"+today.getSeconds();
+}
 function depositar(){
     const valorDeposito = parseFloat(document.getElementById("input-deposito").value)
     if(valorDeposito !== valorDeposito){
         alert("Por favor insira um valor para o depósito")
         return
     }
+    console.log(day)
     console.log(valorDeposito)
     saldo += valorDeposito
     numeroDiv.innerHTML=`
     <span><p> Saldo atual: <strong>R$${saldo}</strong></span> <br>
     `
+    time()
     extratoDiv.innerHTML += `
     <p id="deposito">
         ${day} ${hours} <br><span class="valor-extrato">+R$${valorDeposito}</span>
@@ -40,6 +46,7 @@ function sacar(){
     numeroDiv.innerHTML=`
     <span><p> Saldo atual: <strong>R$${saldo}</strong></span> <br>
     `
+    time()
     extratoDiv.innerHTML += `
     <p id="saque">
         ${day} ${hours} <br><span class="valor-extrato">-R$${valorSaque}</span>
