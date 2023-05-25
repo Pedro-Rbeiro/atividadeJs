26109f33296919721fa3b1c01cfe2f5756a0e6b4
pedro-rbeiro <business.psycho@outlook.com>
2023-05-25 12:10:21 -0300

    Atividade Dark UX

---
+++ atividade dark ux/js/script.js
@@ -0,0 +1,56 @@
+timer()
+function timer() {
+    var countDownDate = new Date("May 31, 2023 15:37:25").getTime();
+    var countDownDateValentine = new Date("Jun 31, 2023 15:37:25").getTime();
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var distance2 = countDownDateValentine - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        var daysValentine = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+        var hoursValentine = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutesValentine = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+        var secondsValentine = Math.floor((distance2 % (1000 * 60)) / 1000);
+        document.getElementById("date").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s "
+        document.getElementById("date-valentine").innerHTML = daysValentine + "d " + hoursValentine + "h "
+            + minutesValentine + "m " + secondsValentine + "s "
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("date").innerHTML = "EXPIRED";
+        }
+        if (distance2 < 0) {
+            clearInterval(x);
+            document.getElementById("date-valentine").innerHTML = "EXPIRED";
+        }
+    }, 1000)
+}
+
+function change1() {
+    var activePhoto = document.getElementById("active-photo").src = "../img/pic1.webp"
+    activePhoto
+}
+function change2() {
+    var activePhoto = document.getElementById("active-photo").src = "../img/pic2.webp"
+    activePhoto
+}
+function change3() {
+    var activePhoto = document.getElementById("active-photo").src = "../img/pic3.webp"
+    activePhoto
+}
+function change4() {
+    var activePhoto = document.getElementById("active-photo").src = "../img/pic4.webp"
+    activePhoto
+}
+function change5() {
+    var activePhoto = document.getElementById("active-photo").src = "../img/pic5.webp"
+    activePhoto
+}
