e6ef98bd47ed60cc8ba6c647a86b68897d0280eb
pedro-rbeiro <business.psycho@outlook.com>
2023-10-04 12:27:19 -0300

    Atividade sequelize

---
+++ aula sequelize/models/User.js
@@ -0,0 +1,27 @@
+const {DataTypes} = require('sequelize')
+const db = require('../db/conn')
+
+const User = db.define('User', {
+    nome:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    email:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    senha:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    ocupacao:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    alertas:{
+        type: DataTypes.BOOLEAN,
+    }
+    
+})
+
+module.exports = User
